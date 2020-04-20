652e5e16dfef2aca0b0a2360e09c926b54bfb4b7
Mike Sisk <msisk@lixar.com>
2020-04-20 08:08:43 -0400

    Creating distribution package

---
+++ tbsdq/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tbsdq-pkg-msisktbs", # Replace with your own username
+    version="0.0.1",
+    author="Mike Sisk",
+    author_email="Mike.Sisk@tbs-sct.gc.ca",
+    description="A library for calculating specific data quality metrics on open data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/msisktbs/data",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
